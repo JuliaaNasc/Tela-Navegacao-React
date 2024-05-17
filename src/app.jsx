3484a22518ebbaedd02f911c21fdeb8cc93b4e72
Júlia Nascimento <juliavcostan@gmail.com>
2024-05-17 13:17:39 -0300

    Colando imagem de background-image no projeto

--- src/app.jsx
+++ src/app.jsx
@@ -6,12 +6,15 @@ export function App() {
 
   return (
     <div className='Cabecalho'>
-      <form>
-        
-      <h1>Seja muito bem vindo ao site da Júlia ❤️</h1>
-      <p>Estou muito feliz em te receber por aqui</p>
-      <button>Entrar</button>
-    
+
+      <img src="./assets/julia_model.png" alt="" />
+
+      <form className='Container'>
+
+        <h1>Seja muito bem vindo ao site da Júlia ❤️</h1>
+        <p>Estou muito feliz em te receber por aqui</p>
+        <button>Entrar</button>
+
       </form>
 
     </div>
