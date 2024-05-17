b129fd15b50a5463394f8217028ea1befda998b5
Júlia Nascimento <juliavcostan@gmail.com>
2024-05-17 12:30:15 -0300

    Tela de Boas-vindas ao usuário dentro de uma platafora

---
+++ src/app.jsx
@@ -0,0 +1,20 @@
+
+import './app.css'
+
+export function App() {
+
+
+  return (
+    <div className='Cabecalho'>
+      <form>
+        
+      <h1>Seja muito bem vindo ao site da Júlia ❤️</h1>
+      <p>Estou muito feliz em te receber por aqui</p>
+      <button>Entrar</button>
+    
+      </form>
+
+    </div>
+
+  )
+}
